8f79267d47a9cc19db5c6574c95de177afeae050
mr9 asif <asifalibd002@gmail.com>
2024-11-30 14:56:56 +0600

    added arr & str problem

--- randomProblem.js
+++ randomProblem.js
@@ -1,7 +1,18 @@
 
  
-const f1 = ()=> 2;
-const f2 = ()=>4;
+// const f1 = ()=> 2;
+// const f2 = ()=>4;
 
-const a = (f1(), f2());
-console.log(a);
+// const a = (f1(), f2());
+ // console.log(a);  4
+// because of replace the value , a can only store one value so 2 replace by 4;
+
+
+const arr = ['one', 'two', 'three'];
+const str = 'Hello';
+
+const res1 = arr.includes('on');
+const res2 = arr.includes('onetwo');
+const res3 = str.includes('ll');
+
+console.log(res1, res2, res3)
