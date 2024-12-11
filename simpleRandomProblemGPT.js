f8c636ea0560ce97e688985a7cf424507838b149
mr9 asif <asifalibd002@gmail.com>
2024-12-11 21:19:10 +0600

    added problem6

--- simpleRandomProblemGPT.js
+++ simpleRandomProblemGPT.js
@@ -89,5 +89,17 @@
 //   console.log(spiralOrder([[1, 2, 3], [4, 5, 6], [7, 8, 9]]));
 //   // Output: [1, 2, 3, 6, 9, 8, 7, 4, 5]
 
+// Problem: Implement a curried function.
+// function curry(fn) {
+//     return function curried(...args) {
+//       return args.length >= fn.length
+//         ? fn(...args)
+//         : (...next) => curried(...args, ...next);
+//     };
+//   }
+//   const add = (a, b, c) => a + b + c;
+//   const curriedAdd = curry(add);
+//   console.log(curriedAdd(1)(2)(3)); // Output: 6
+  
 
   
