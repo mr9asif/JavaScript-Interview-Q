70f957a1dd154d1f3da0e936a04a0d0dce1932b0
mr9 asif <asifalibd002@gmail.com>
2024-12-11 21:16:10 +0600

    added problem4

--- simpleRandomProblemGPT.js
+++ simpleRandomProblemGPT.js
@@ -56,3 +56,24 @@
 //   console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
 //   // Output: [["eat", "tea", "ate"], ["tan", "nat"], ["bat"]]
   
+// Problem: Find the first and last index of a target in a sorted array.
+// function searchRange(nums, target) {
+//     function findBound(isFirst) {
+//       let left = 0, right = nums.length - 1, bound = -1;
+//       while (left <= right) {
+//         const mid = Math.floor((left + right) / 2);
+//         if (nums[mid] === target) {
+//           bound = mid;
+//           isFirst ? right = mid - 1 : left = mid + 1;
+//         } else if (nums[mid] < target) {
+//           left = mid + 1;
+//         } else {
+//           right = mid - 1;
+//         }
+//       }
+//       return bound;
+//     }
+//     return [findBound(true), findBound(false)];
+//   }
+//   console.log(searchRange([5, 7, 7, 8, 8, 10], 8)); // Output: [3, 4]
+  
