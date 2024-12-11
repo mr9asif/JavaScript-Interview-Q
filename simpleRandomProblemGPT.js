159f9a5ead15359e392aa62476b5993a255389ac
mr9 asif <asifalibd002@gmail.com>
2024-12-11 21:23:05 +0600

    added problem7

--- simpleRandomProblemGPT.js
+++ simpleRandomProblemGPT.js
@@ -101,5 +101,16 @@
 //   const curriedAdd = curry(add);
 //   console.log(curriedAdd(1)(2)(3)); // Output: 6
   
+// Problem: Find the first non-repeating character in a string.
+// function firstUniqChar(s) {
+//     const map = {};
+//     for (let char of s) map[char] = (map[char] || 0) + 1;
+//     for (let i = 0; i < s.length; i++) {
+//       if (map[s[i]] === 1) return i;
+//     }
+//     return -1;
+//   }
+//   console.log(firstUniqChar("loveleetcode")); // Output: 2
+  
 
   
