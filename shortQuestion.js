1a585ab127a7776fbbe96afc1fb8010bba3d1b75
mr9 asif <asifalibd002@gmail.com>
2024-12-12 20:55:33 +0600

    added simple q1

---
+++ shortQuestion.js
@@ -0,0 +1,13 @@
+// What is a closure in JavaScript?
+// Ans: A closure is a function that retains access to its lexical scope, even when the function is executed
+// function outer() {
+//     let count = 0;
+//     return function inner() {
+//       count++;
+//       console.log(count);
+//     };
+//   }
+//   const counter = outer();
+//   counter(); // 1
+//   counter(); // 2
+  
