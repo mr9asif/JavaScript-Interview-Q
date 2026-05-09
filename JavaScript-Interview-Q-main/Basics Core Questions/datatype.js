a2c089aeb8339b79731267f6481e0e16538d0ce4
mr9 asif <asifalibd002@gmail.com>
2026-05-09 19:49:57 +0600

    added datatypes

---
+++ JavaScript-Interview-Q-main/Basics Core Questions/datatype.js
@@ -0,0 +1,27 @@
+// Under all are Falsy value--------
+/*
+false
+0
+-0
+0n
+""
+null
+undefined
+NaN
+
+*/
+
+
+// under all are Truthy values----------->
+ /* []
+ {}
+ "0"
+"false"
+
+*/
+
+// some js weired things--------->
+/*
+typeof NaN // "number"
+NaN === NaN // false
+*/
